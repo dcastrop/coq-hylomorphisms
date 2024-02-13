efb646ac35bfbd1dee726573e55e6cdcc95ac8f5
David Castro-Perez <david.castro.dcp@gmail.com>
2024-02-13 18:26:11 +0000

    Containers and polynomial functors

--- theories/Container.v
+++ theories/Container.v
@@ -117,49 +117,6 @@ Defined.
 Lemma fold_eq A (x y : A) : x =e y -> x = y.
 Proof. trivial. Qed.
 
-Definition ComposeDom `(F : Container S1 P1) `(G : Container S2 P2) :
-  @morph (App F S2 * (P1 * P2))
-    (pair_eq (eq_B := def_eq (P1 * P2))) bool (def_eq bool).
-  refine
-    {| app :=
-        fun (sp : App F S2 * (P1 * P2)) =>
-          let (s , p12) := sp in
-          let (p1 , p2) := p12 in
-          match dom (shape s, p1) as b
-                return dom (shape s, p1) = b -> bool
-          with
-          | true => fun H =>
-                      let e := MkElem p1 H in
-                      let kp := cont s e in
-                      dom (kp, p2)
-          | false => fun _ => false
-          end eq_refl
-    |}.
-  intros [a [pa1 pa2]] [b [pb1 pb2]] [aRb E12]; simpl in *.
-  inversion E12. rename H0 into E1. rename H1 into E2.
-  destruct aRb as [Es Ek].
-  assert (Dab : dom (shape a, pa1) =e dom (shape b, pb1)).
-  { apply app_eq. split; trivial. }
-  simpl in Dab. subst.
-  generalize (@eq_refl _ (dom (shape a, pb1))).
-  generalize (@eq_refl _ (dom (shape b, pb1))).
-  revert Dab.
-  generalize (dom (shape a, pb1)) at 1 3 4. intros []; simpl in *.
-  + generalize (dom (shape b, pb1)) at 1 3 4. intros []; try discriminate.
-    intros _ d1 d2. apply fold_eq, app_eq. split; simpl in *; trivial.
-    apply Ek; trivial.
-  + generalize (dom (shape b, pb1)) at 1 3 4. intros []; try discriminate.
-    trivial.
-Defined.
-Arguments ComposeDom {S1 Esh P1} F {S2 Esh0 P2} G.
-
-#[export]
-  Instance CompCont `(F : Container S1 P1) `(G : Container S2 P2) :
-  Container (App F S2) (P1 * P2) :=
-  {
-    dom := ComposeDom F G
-  }.
-
 Lemma cont_ext_eq `{F : Container Sh P} (s : Sh) `{equiv X}
   (k k' : Pos s -> X)
   : (forall x, k x =e k' x) -> AppR (MkCont s k) (MkCont s k').
@@ -200,3 +157,103 @@ Proof. intros []; reflexivity. Qed.
 Lemma fmap_comp `{F : Container Sh P} `{equiv A} `{equiv B} `{equiv C}
   (f : B ~> C) (g : A ~> B) : fmap (F:=F) (f \o g) =e fmap f \o fmap g.
 Proof. intros []. reflexivity. Qed.
+
+
+(* Polynomial functors as containers *)
+
+(* Identity container *)
+Definition Is := unit.
+Definition Ip := unit.
+Instance Id : Container Is Ip.
+refine {| dom := {| app := fun _ => true|} |}.
+intros. reflexivity.
+Defined.
+
+(* Constant container *)
+Definition Ks (A : Type) := A.
+Definition Kp := False.
+Instance K A : Container (Ks A) Kp.
+refine {| dom := {| app := fun x : Ks A * Kp  => match snd x with end |} |}.
+intros. destruct (snd x).
+Defined.
+
+(* Product container *)
+Section Product.
+  Context `{equiv S1} `{equiv S2} (P1 P2 : Type).
+  Context (C1 : Container S1 P1) (C2 : Container S2 P2).
+  Notation Ps := ((S1 * S2)%type).
+  Notation Pp := ((P1 + P2)%type).
+  Instance Prod : Container Ps Pp.
+  refine {| dom := {| app := fun x : Ps * Pp  =>
+                               match snd x with
+                               | inl p => dom (fst (fst x), p)
+                               | inr p => dom (snd (fst x), p)
+                               end |} |}.
+  intros [[??]?][[??]p0] [[??]?]. simpl in *; subst.
+  destruct p0 as [p|p].
+  - apply fold_eq, app_eq. simpl; split; trivial.
+  - apply fold_eq, app_eq. simpl; split; trivial.
+  Defined.
+End Product.
+
+(* Sum container *)
+Section Sum.
+  Context `{equiv S1} `{equiv S2} (P1 P2 : Type).
+  Context (C1 : Container S1 P1) (C2 : Container S2 P2).
+  Notation Ps := ((S1 + S2)%type).
+  Notation Pp := ((P1 + P2)%type).
+  Instance Sum : Container Ps Pp.
+  refine {| dom := {| app := fun x : Ps * Pp  =>
+                               match fst x, snd x with
+                               | inl s, inl p => dom (s, p)
+                               | inr s, inr p => dom (s, p)
+                               | _, _ => false
+                               end |} |}.
+  intros [[?|?][?|?]] [[?|?][?|?]] [E F];
+    simpl in *; subst; try (inversion F); try (inversion E); trivial.
+  - apply fold_eq, app_eq. simpl; split; trivial.
+  - apply fold_eq, app_eq. simpl; split; trivial.
+  Defined.
+End Sum.
+
+Section Composition.
+  Context `(F : Container S1 P1) `(G : Container S2 P2).
+
+  Definition composeDom :
+    @morph (App F S2 * (P1 * P2))
+      (pair_eq (eq_B := def_eq (P1 * P2))) bool (def_eq bool).
+    refine
+      {| app :=
+          fun (sp : App F S2 * (P1 * P2)) =>
+            let (s , p12) := sp in
+            let (p1 , p2) := p12 in
+            match dom (shape s, p1) as b
+                  return dom (shape s, p1) = b -> bool
+            with
+            | true => fun H =>
+                        let e := MkElem p1 H in
+                        let kp := cont s e in
+                        dom (kp, p2)
+            | false => fun _ => false
+            end eq_refl
+      |}.
+    intros [a [pa1 pa2]] [b [pb1 pb2]] [aRb E12]; simpl in *.
+    inversion E12. rename H0 into E1. rename H1 into E2.
+    destruct aRb as [Es Ek].
+    assert (Dab : dom (shape a, pa1) =e dom (shape b, pb1)).
+    { apply app_eq. split; trivial. }
+    simpl in Dab. subst.
+    generalize (@eq_refl _ (dom (shape a, pb1))).
+    generalize (@eq_refl _ (dom (shape b, pb1))).
+    revert Dab.
+    generalize (dom (shape a, pb1)) at 1 3 4. intros []; simpl in *.
+    + generalize (dom (shape b, pb1)) at 1 3 4. intros []; try discriminate.
+      intros _ d1 d2. apply fold_eq, app_eq. split; simpl in *; trivial.
+      apply Ek; trivial.
+    + generalize (dom (shape b, pb1)) at 1 3 4. intros []; try discriminate.
+      trivial.
+  Defined.
+
+  #[export]
+    Instance CompCont : Container (App F S2) (P1 * P2) := { dom := composeDom }.
+End Composition.
