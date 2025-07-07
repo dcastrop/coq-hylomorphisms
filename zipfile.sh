1d60006be4f17c734c270a1545aa8bd1076be508
David Castro-Perez <david.castro.dcp@gmail.com>
2025-07-07 18:01:21 +0100

    git archive for paper

--- zipfile.sh
+++ zipfile.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
 rm -rf ./archive && mkdir archive
-git archive --format zip --prefix coq-hylos/ --worktree-attributes HEAD -o archive/coq-hylos.zip
+git archive --format zip --worktree-attributes HEAD -o archive/coq-hylos.zip
